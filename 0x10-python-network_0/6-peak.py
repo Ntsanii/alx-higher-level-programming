8d8b3be96c7855d6ea7d647f065e01f3e9173e79
Ntsanii <ntsane01m@gmail.com>
2024-07-04 16:32:48 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+""" a function that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """ find the peak number with the shortest algorithm"""
+    if list_of_integers == []:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
+
+    size = len(list_of_integers)
+    if size == 0:
+        return (None)
+    elif size == 1:
+        return (list_of_integers[0])
+    elif size == 2:
         return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    mid = int(size/2)
+    peak = list_of_integers[mid]
+    mylist = list_of_integers
+    if peak > mylist[mid - 1] and peak > mylist[mid + 1]:
+        return peak
+    elif peak < mylist[mid - 1]:
+        return find_peak(mylist[:mid])
+    else:
+        return find_peak(mylist[mid + 1:])
