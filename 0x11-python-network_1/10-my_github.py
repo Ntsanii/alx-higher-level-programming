0ebf9e21b011d12eca3f5a857b004c1cf93a4861
Ntsanii <ntsane01m@gmail.com>
2024-07-05 10:35:38 -0700

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Takes your GitHub credentials (username and password) and uses the GitHub API"""
+"""Python script that takes your GitHub credentials and uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
 
-if __name__ == '__main__':
-    from requests import get
-    from sys import argv
+    url = "https://api.github.com/user"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    data_res = requests.get(url, auth=(user, passwd))
 
-    username = argv[1]
-    password = argv[2]
-
-    URL = "https://api.github.com/user"
-    response = get(URL, auth=(username, password))
-    json = response.json()
-
-    print(json.get('id'))
+    try:
+        data_json = data_res.json()
+        print(data_json["id"])
+    except Exception:
+        print("None")
